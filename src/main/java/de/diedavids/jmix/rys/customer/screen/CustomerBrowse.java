b39b361c911dc454e18bff1570461c254948e71f
Mario David <mario@die-davids.de>
2022-01-17 09:06:23 +0100

    create customer entity & screens (#13) - fixes #8

---
+++ src/main/java/de/diedavids/jmix/rys/customer/screen/CustomerBrowse.java
@@ -0,0 +1,10 @@
+package de.diedavids.jmix.rys.customer.screen;
+
+import io.jmix.ui.screen.*;
+import de.diedavids.jmix.rys.customer.Customer;
+
+@UiController("rys_Customer.browse")
+@UiDescriptor("customer-browse.xml")
+@LookupComponent("customersTable")
+public class CustomerBrowse extends StandardLookup<Customer> {
+}
