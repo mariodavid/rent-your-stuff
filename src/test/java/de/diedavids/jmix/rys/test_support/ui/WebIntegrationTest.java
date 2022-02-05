3f1cb5e95045caeb8e897d3f0f60d0248121de8d
mariodavid <mario@die-davids.de>
2022-02-05 10:28:12 +0100

    fix flakyness of UI tests

--- src/test/java/de/diedavids/jmix/rys/test_support/ui/WebIntegrationTest.java
+++ src/test/java/de/diedavids/jmix/rys/test_support/ui/WebIntegrationTest.java
@@ -2,8 +2,11 @@ package de.diedavids.jmix.rys.test_support.ui;
 
 
 import de.diedavids.jmix.rys.RentYourStuffApplication;
+import de.diedavids.jmix.rys.product.Product;
+import io.jmix.ui.Screens;
 import io.jmix.ui.testassist.UiTestAssistConfiguration;
 import io.jmix.ui.testassist.junit.UiTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,4 +16,10 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(classes = {RentYourStuffApplication.class, UiTestAssistConfiguration.class})
 @AutoConfigureTestDatabase
 public abstract class WebIntegrationTest {
+
+
+    @BeforeEach
+    void removeAllScreens(Screens screens) {
+        screens.removeAll();
+    }
 }
